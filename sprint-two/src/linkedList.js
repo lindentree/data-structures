896bf52dbd5da0709ad2cefb1e8c265261d9dbc2
lindentree <hchiu87@gmail.com>
2018-11-08 15:58:05 -0800

    added linkedlist implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+      if(this.tail) {
+        this.tail.next = newNode
+      } else {
+        this.head = newNode
+      }
+
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+    var formerValue = list.head.value;
+    list.head = list.tail;
+    return formerValue;
   };
 
   list.contains = function(target) {
+    if (this.head.value === target || this.tail.value === target) {
+        
+      return true;
+        
+    } else if(this.head.next === null || this.tail.next === null) {
+      
+        return false
+       
+    } else {
+        list.contains(this.next);
+      } 
+
   };
 
   return list;

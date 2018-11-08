03c218bbca8c82a62647ce18c00a86c508856d6b
Linden Chiu <hchiu87@gmail.com>
2018-11-08 00:07:15 -0800

    added pseudoclassical implementation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,39 @@
 var Queue = function() {
+
+  this.lowest = 0;
+  this.position = 0;
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 
+Queue.prototype.size = function() {
+    var size = this.position - this.lowest
+  	if (size < 1) {
+  		return 0;
+  	}
+  	
+  	return size;
+ };
+  Queue.prototype.enqueue = function (value) {
+  	var key = this.position
+  	
+  	this[key] = value;
+  
+    this.position++;
+  };
+  Queue.prototype.dequeue = function (value) {
+  	var idx = this.lowest 
+
+  	var x = idx.toString();
+  	
+  	var popped = this[x];
+  	//console.log(popped)
+  	delete this[x];
+  	this.lowest++;
+  	//this.size--;
+  	return popped;
+  	
+  };
+var someInstance = new Queue;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,33 @@
 var Stack = function() {
+
+  this.position = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 
+Stack.prototype.size= function() {
+  	if (this.position < 0) {
+  		return 0;
+  	}
+  	return this.position;
+  };
+  Stack.prototype.push= function (value) {
+  	var key = this.position
+  	
+  	this[key] = value;
+  
+    this.position++;
+  };
+  Stack.prototype.pop = function (value) {
+  	var key = this.position - 1
+  	var x = key.toString();
+  	console.log(this)
+  	var popped = this[x];
+  	delete this[x];
+  	this.position--;
+  	return popped;
+  	
+  }
+
+  var someInstance = new Stack;

31fde620227a43a4a01d9c88f843d79f7fb7a3db
lindentree <hchiu87@gmail.com>
2018-11-08 16:04:55 -0800

    started tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,11 @@
+//what is tree?
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  
   // your code here
   newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 

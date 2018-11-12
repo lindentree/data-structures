9b3e22b2b0549be94f89701f63b11d405eafbd20
Linden Chiu <hchiu87@gmail.com>
2018-11-11 17:16:56 -0800

    implement pseudoclassical instantiation and insert method for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,31 @@
 var BinarySearchTree = function(value) {
+  
+  this.value = value;
+  this.left = null;
+  this.right = null;
 	
 };
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value < this.value && this.left === null) {
+  	this.left = new BinarySearchTree(value);
+  	
+  } else if (value > this.value && this.right === null) {
+  	  this.right = new BinarySearchTree(value);
+    } else if (value < this.value && this.left !== null) {
+  	    this.left.insert(value);
+  	 } else {
+  	     this.right.insert(value);
+  	 }
+}
+
+BinarySearchTree.prototype.contains = function (value) {
+	
+}
+
+BinarySearchTree.prototype.depthFirstLog = function (value) {
+	
+}
 
 
 /*
